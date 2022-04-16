433d3ba53a3f717ab698d2d8df3d1886bb14164c
vuong.exnodes <vuongtechnique2000@gmail.com>
2022-04-16 16:12:31 +0700

    client-home-recent_news

--- client/src/pages/Home/RecentsNew/RecentsNew.jsx
+++ client/src/pages/Home/RecentsNew/RecentsNew.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import classes from './styles.module.scss';
-
+import React from "react";
+import classes from "./styles.module.scss";
+import watch from "../../../assets/home/recentNews/Mask Group.png";
+import vawe1 from "../../../assets/home/Vector 12.png";
+import vawe2 from "../../../assets/home/Vector 13.png";
+import vawe3 from "../../../assets/home/Vector 14.png";
 const RecentsNew = () => {
   return (
-    <div>RecentsNew</div>
-  )
-}
+    <div className={classes.recentNewContainer}>
+      <div className={classes.mainTitle}>Recent News</div>
+      <div className={classes.recentNews}>
+        <div className={classes.left}>
+          <div className={classes.content}>
+            <div className={classes.where}>Where To Travel</div>
+            <div className={classes.title}>
+              Matoa Where To Travel? Ho Chi Minh City
+            </div>
+            <button className={classes.btn}>Discover</button>
+          </div>
+        </div>
+        <div className={classes.right}>
+          <img src={watch} alt="" />
+        </div>
+        <div className={classes.square}></div>
+        <div className={classes.image}>
+          <img src={vawe1} alt="" />
+          <img src={vawe2} alt="" />
+          <img src={vawe3} alt="" />
+        </div>
+        <div className={classes.image1}>
+          <img src={vawe1} alt="" />
+          <img src={vawe2} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RecentsNew
+export default RecentsNew;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "./Carousel/Carousel";
 import Category from "./Category/Category";
 import MonthlyDeal from './MonthlyDeal/MonthlyDeal'
+import RecentsNew from "./RecentsNew/RecentsNew";
 import classes from './styles.module.scss';
 
 const Home = () => {
@@ -18,7 +19,16 @@ const Home = () => {
       <MonthlyDeal></MonthlyDeal>
       </div>
       <div className={classes.recentsNew}>
-        
+       <RecentsNew></RecentsNew> 
+      </div>
+      <div className={classes.featured}>
+       <RecentsNew></RecentsNew> 
+      </div>
+      <div className={classes.testimonial}>
+       <RecentsNew></RecentsNew> 
+      </div>
+      <div className={classes.instagram}>
+       <RecentsNew></RecentsNew> 
       </div>
     </div>
   );
