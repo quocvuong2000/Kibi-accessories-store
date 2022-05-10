22bd8dfacff4e576c0c5bc85f3b95b9c43951b49
vuong.exnodes <vuongtechnique2000@gmail.com>
2022-05-10 21:55:38 +0700

    api-first-attempt

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+
+dotenv.config();
+
