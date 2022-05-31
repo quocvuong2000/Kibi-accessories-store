b5d23a35c1adbfa0c728095755640c6538fed0ba
ngduonganhhuy <ngduonganhhuy@gmail.com>
2022-05-31 18:33:08 +0700

    file store

---
+++ api/models/Address.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const AddressSchema = new mongoose.Schema(
+  {
+    street: { type: String },
+    ward: { type: String },
+    district: { type: String },
+    city: { type: String },
+    user: [
+      {
+        userId: { type: String, required: true },
+      },
+    ],
+    fullAddress: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Address", AddressSchema);
