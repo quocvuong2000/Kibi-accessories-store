31e5e48ec7ad87b2cbbbb3913edbcfe96add9a81
ngduonganhhuy <ngduonganhhuy@gmail.com>
2022-06-24 23:08:38 +0700

    blog, address, comment

---
+++ client/src/pages/Blog/Card/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Card = ({ title, content }) => (
+  <div className="card">
+    <h2>{title}</h2>
+    <p>{content}</p>
+  </div>
+);
+
+export default Card;
--- client/src/pages/Blog/index.jsx
+++ client/src/pages/Blog/index.jsx
@@ -3,10 +3,21 @@ import s from "./styles.module.scss";
 import blogmain from "../../assets/blogmain.jpg";
 import { Link } from "react-router-dom";
 import DetailAuthor from "./DetailAuthor";
+import Carousel from "./Carousel";
+import Card from "./Card";
+const CARDS = 10;
 
 const Blog = () => {
   return (
     <div className={s.blog}>
+      <Carousel>
+        {[...new Array(CARDS)].map((_, i) => (
+          <Card
+            title={"Card " + (i + 1)}
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+        ))}
+      </Carousel>
       <div className={s.blog_img_main}>
         <img src={blogmain} alt="" />
         <div className={s.blog_general}>
