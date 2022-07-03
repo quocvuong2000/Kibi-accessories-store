f56928922061ed5c33f80f4c193ba77071a399ba
vuong.exnodes <vuongtechnique2000@gmail.com>
2022-07-03 13:50:40 +0700

    fix-cart

--- api/models/Order.js
+++ api/models/Order.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String },
-  products: [
-    {
-      _id: false,
-      productId: { type: String },
-      productName: { type: String },
-      productImage: { type: Array },
-      productPrice: { type: Number },
-      quantity: { type: Number },
-    },
-  ],
-  status: { type: String },
-  totalProduct: { type: Number },
-  totalPrice: { type: Number },
-  shippingPrice: { type: Number },
-  amount: { type: Number },
-  address: { type: String },
-  recipientName: { type: String },
-  recipientPhone: { type: Number, required: true },
-  paid: { type: Boolean },
-});
+const OrderSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String },
+    products: [
+      {
+        _id: false,
+        productId: { type: String },
+        productName: { type: String },
+        productImage: { type: Array },
+        productPrice: { type: Number },
+        quantity: { type: Number },
+      },
+    ],
+    status: { type: String },
+    totalProduct: { type: Number },
+    totalPrice: { type: Number },
+    shippingPrice: { type: Number },
+    amount: { type: Number },
+    address: { type: String },
+    recipientName: { type: String },
+    recipientPhone: { type: Number, required: true },
+    paid: { type: Boolean },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", OrderSchema);
