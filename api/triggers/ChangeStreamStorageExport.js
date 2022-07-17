3538b8e26ba30e5bfd45a4285c5e631d35ef1867
vuong.exnodes <vuongtechnique2000@gmail.com>
2022-07-17 13:21:17 +0700

    update

--- api/triggers/ChangeStreamStorageExport.js
+++ api/triggers/ChangeStreamStorageExport.js
@@ -7,43 +7,91 @@ async function monitorStorageExport(client, timeInMs) {
   const pipeline = [
     {
       $match: {
-        operationType: "update",
-        "updateDescription.updatedFields.status": "COMPLETED",
+        $or: [
+          {
+            operationType: "update",
+            "updateDescription.updatedFields.status": "COMPLETED",
+          },
+          {
+            operationType: "insert",
+            "updateDescription.updatedFields.status": "PENDING",
+          },
+          {
+            operationType: "update",
+            "updateDescription.updatedFields.status": "CANCELLED",
+          },
+        ],
       },
     },
   ];
   const changeStream = collection.watch(pipeline);
 
   changeStream.on("change", async (next) => {
-    const orderFound = await Order.findById(next.documentKey._id);
+    if (next.updateDescription.updatedFields.status === "PENDING") {
+      const orderFound = await Order.findById(next.documentKey._id);
+      orderFound.products.forEach(async (el) => {
+        const productDetail = await Product.findById(el.productId);
+        const branchFound = productDetail.branches.find(
+          (el) => el.branchId.toString() === orderFound.branchId
+        );
 
-    orderFound.products.forEach(async (el) => {
-      if (mongoose.Types.ObjectId.isValid(el.productId)) {
-        const oldQuantity = await Product.findById(el.productId);
-
-        if (oldQuantity && oldQuantity.quantity > 0) {
-          const newQuantity = oldQuantity.quantity - el.quantity;
-          const newExport = {
-            branchId: orderFound.branchId || "NA",
-            productId: el.productId,
-            newQuantity: newQuantity,
-            oldQuantity: oldQuantity.quantity,
-            branchName: orderFound.branchName || "NA",
-            productName: el.productName,
-            status: "Export",
-          };
-          try {
-            const savedStorage = new Storage(newExport);
-            await savedStorage.save();
-            await Product.findByIdAndUpdate(el.productId, {
-              quantity: newQuantity,
+        const newBranchesArr = [];
+        productDetail.branches.forEach((proEl) => {
+          if (proEl.branchId.toString() === orderFound.branchId) {
+            newBranchesArr.push({
+              branchId: branchFound.branchId,
+              branchName: branchFound.branchName,
+              quantity: branchFound.quantity - parseInt(el.quantity),
+              oldQuantity: branchFound.quantity,
             });
-          } catch (error) {
-            console.log(error);
+          } else {
+            newBranchesArr.push(proEl);
+          }
+        });
+        try {
+          await Product.findByIdAndUpdate(el.productId, {
+            branches : newBranchesArr,
+            oldQuantity : productDetail.quantity,
+            quantity : productDetail.quantity - el.quantity
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    }
+
+    if (next.updateDescription.updatedFields.status === "CANCELLED") {
+    }
+    if (next.updateDescription.updatedFields.status === "COMPLETE") {
+      const orderFound = await Order.findById(next.documentKey._id);
+      orderFound.products.forEach(async (el) => {
+        if (mongoose.Types.ObjectId.isValid(el.productId)) {
+          const oldQuantity = await Product.findById(el.productId);
+
+          if (oldQuantity && oldQuantity.quantity > 0) {
+            const newQuantity = oldQuantity.quantity - el.quantity;
+            const newExport = {
+              branchId: orderFound.branchId || "NA",
+              productId: el.productId,
+              newQuantity: newQuantity,
+              oldQuantity: oldQuantity.quantity,
+              branchName: orderFound.branchName || "NA",
+              productName: el.productName,
+              status: "Export",
+            };
+            try {
+              const savedStorage = new Storage(newExport);
+              await savedStorage.save();
+              await Product.findByIdAndUpdate(el.productId, {
+                quantity: newQuantity,
+              });
+            } catch (error) {
+              console.log(error);
+            }
           }
         }
-      }
-    });
+      });
+    }
   });
 
   // await closeChangeStream(timeInMs, changeStream);
